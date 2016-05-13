931bf1b77a2a44c3b5928ce8153c1800ef9feca3
keita03301995 <keita03301995@gmail.com>
2016-05-13 22:59:46 +0900

    ディレクトリ階層の修正

--- dotfiles/dotfilesInstaller.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-ln -s ~/dotfiles/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/dotfiles/.gitignore ~/.gitignore
-ln -s ~/dotfiles/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/dotfiles/etc ~/etc
---
+++ dotfilesInstaller.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/etc ~/etc
